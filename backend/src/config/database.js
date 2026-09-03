35d5c10f49a7b9dd26f52a330864a0c904367f28
anjanyelles <anjanyelles@gmail.com>
2026-09-03 16:53:24 +0530

    Add connection pool settings for production database stability

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,6 +4,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  max: 20, // Maximum number of clients in the pool
+  idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
+  connectionTimeoutMillis: 10000, // Return an error after 10 seconds if connection could not be established
 });
 
 module.exports = pool;
