2356b6a550dbd02b2ac0cec8126a25deebbe7e97
anjanyelles <anjanyelles@gmail.com>
2026-09-03 13:33:40 +0530

    changes are mode

--- frontend/services/cartApi.js
+++ frontend/services/cartApi.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:3000';
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 async function handleResponse(res) {
   const data = await res.json().catch(() => ({}));
